790020723022754290ba6651f86222e0b2727531
ChenWei <472484497@qq.com>
2019-04-18 05:57:04 -0700

    update

--- Search/setup.py
+++ Search/setup.py
@@ -2,10 +2,10 @@
 
 from setuptools import setup, find_packages
 setup(
-    name = "Search",
+    name = "search",
     version = "0.1",
     author = "Cris",
-    packages = find_packages(),
+    #packages = find_packages(),
     install_requires = ["BeautifulSoup4", "requests", "urwid"],
     python_requires=">=3",
     include_package_data=True,
